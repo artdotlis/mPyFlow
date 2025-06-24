76b0637084c3d1ea45614ac229e719c8e4da12d1
Artur Lissin <artdotlis@pm.me>
2025-06-24 13:46:21 +0200

    fix: correct test syncqueue types

--- tests/fixture/workable.py
+++ tests/fixture/workable.py
@@ -15,8 +15,8 @@ pytest_plugins = ("tests.fixture.context",)
 def two_sync_workables(
     factory_ctx: SpawnContext,
 ) -> tuple[Workable[str, str], Workable[str, str]]:
-    que_1 = SyncQueue[str, str](factory_ctx, 2, str_to_bytes, bytes_to_str)
-    que_2 = SyncQueue[str, str](factory_ctx, 2, str_to_bytes, bytes_to_str)
+    que_1 = SyncQueue[str](factory_ctx, 2, str_to_bytes, bytes_to_str)
+    que_2 = SyncQueue[str](factory_ctx, 2, str_to_bytes, bytes_to_str)
     return (
         Workable[str, str](factory_ctx, PassThrough(), que_1),
         Workable[str, str](factory_ctx, PassThrough(), que_2),

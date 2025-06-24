488b7c16afec511119aa0c3ff1910c980a8386cd
Artur Lissin <artdotlis@pm.me>
2025-06-24 15:55:39 +0200

    refactor: reduce output to console

--- src/mpyflow/library/work/pass_through.py
+++ src/mpyflow/library/work/pass_through.py
@@ -5,13 +5,19 @@ from typing import AsyncIterator, final, Any
 
 @final
 class PassThrough:
-    __slots__: tuple[str, ...] = tuple()
+    __slots__: tuple[str, ...] = ("__output",)
 
-    def on_close(self, _sync_out: SyncStdoutInterface, /) -> None:
-        pass
+    def __init__(self, output: bool = False) -> None:
+        super().__init__()
+        self.__output = output
+
+    def on_close(self, sync_out: SyncStdoutInterface, /) -> None:
+        if self.__output:
+            sync_out.print_message("", "worker: finished")
 
     async def work(
         self, sync_out: SyncStdoutInterface, data: Any, _th_exc: ThreadPoolExecutor, /
     ) -> AsyncIterator[Any]:
-        sync_out.print_message("", f"worker: {data!s}")
+        if self.__output:
+            sync_out.print_message("", f"worker: {data!s}")
         yield data

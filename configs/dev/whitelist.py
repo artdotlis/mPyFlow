651f6fbec0b4d3e12de0e2842e07058169d06132
Artur Lissin <artdotlis@pm.me>
2025-09-25 22:41:25 +0000

    build: add vulture whitelist

---
+++ configs/dev/whitelist.py
@@ -0,0 +1,7 @@
+# type: ignore
+# ruff: noqa
+thread_pool  # unused variable (src/mpyflow/shared/interfaces/io.py:16)
+str_to_bytes  # unused function (src/mpyflow/shared/convert/enc_dec.py:1)
+bytes_to_str  # unused function (src/mpyflow/shared/convert/enc_dec.py:5)
+get_worker_ctx  # unused function (src/mpyflow/shared/context/process.py:8)
+start_worker  # unused function (src/mpyflow/run_wrapper.py:153)
